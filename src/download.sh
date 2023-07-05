51a06d1248d617dbb848dd1d086cf4628cb35969
Ali Afghah <ali.afghah@gmail.com>
2023-07-05 16:39:21 +0330

    Update download.sh

--- src/download.sh
+++ src/download.sh
@@ -5,7 +5,7 @@ get_latest_version() {
         url="https://api.github.com/repos/${is_core_repo}/releases/latest?v=$RANDOM"
         ;;
     sh)
-        name="$is_core_name 脚本"
+        name="$is_core_name script"
         url="https://api.github.com/repos/$is_sh_repo/releases/latest?v=$RANDOM"
         ;;
     caddy)
@@ -15,7 +15,7 @@ get_latest_version() {
     esac
     latest_ver=$(_wget -qO- $url | grep tag_name | egrep -o 'v([0-9.]+)')
     [[ ! $latest_ver ]] && {
-        err "获取 ${name} 最新版本失败."
+        err "Failed to get latest version of ${name}."
     }
     unset name url
 }
@@ -38,7 +38,7 @@ download() {
         chmod +x $is_core_bin
         ;;
     sh)
-        name="$is_core_name 脚本"
+        name="$is_core_name script"
         tmpfile=$tmpdir/sh.zip
         link="https://github.com/${is_sh_repo}/releases/download/${latest_ver}/code.zip"
         download_file
@@ -62,6 +62,6 @@ download() {
 download_file() {
     if ! _wget -t 5 -c $link -O $tmpfile; then
         rm -rf $tmpdir
-        err "\n下载 ${name} 失败.\n"
+        err "\nDownload ${name} failed.\n"
     fi
 }
