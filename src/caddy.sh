ee81fe5c77b5580eb17ba6177ce380313756ade0
Ali Afghah <ali.afghah@gmail.com>
2023-07-05 17:44:21 +0330

    Update caddy.sh

--- src/caddy.sh
+++ src/caddy.sh
@@ -6,8 +6,6 @@ caddy_config() {
         cat >$is_caddyfile <<-EOF
 # don't edit this file #
 # for more info, see https://233boy.com/$is_core/caddy-auto-tls/
-# 不要编辑这个文件 #
-# 更多相关请阅读此文章: https://233boy.com/$is_core/caddy-auto-tls/
 import $is_caddy_conf/*.conf
 import $is_caddy_dir/sites/*.conf
 EOF
